bbd90700804f6ed0a9ef1722d62e999665ecb0d1
Jonao6 <xeduardojonatas@gmail.com>
2023-06-01 20:35:27 +0000

    push to my github

--- index.js
+++ index.js
@@ -2,7 +2,14 @@ require('dotenv').config();
 const express = require('express');
 const cors = require('cors');
 const app = express();
+const bodyParser = require('body-parser')
+const mySecret = process.env['MONGO_URI']
+const mongoose = require("mongoose")
 
+mongoose.connect(mySecret, {
+useNewUrlParser: true,
+useUnifiedTopology: true,
+});
 // Basic Configuration
 const port = process.env.PORT || 3000;
 
@@ -21,4 +28,60 @@ app.get('/api/hello', function(req, res) {
 
 app.listen(port, function() {
   console.log(`Listening on port ${port}`);
+});
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
+
+const urlSchema = new mongoose.Schema({
+  original_url: { type: String, required: true },
+  short_url: { type: String, required: true }
+});
+
+const Url = mongoose.model('Url', urlSchema);
+
+let nextShortUrlId = 1;
+
+const urlRegex = /^(http(s):\/\/.)[-a-zA-Z0-9@:%._\+~#=]{2,256}\.[a-z]{2,6}\b([-a-zA-Z0-9@:%_\+.~#?&//=]*)$/;
+
+app.post('/api/shorturl', (req, res) => {
+  const { url } = req.body;
+
+  if (!url) {
+    return res.json({ error: 'Invalid request body. Missing URL.' });
+  }
+
+  if (!urlRegex.test(url)) {
+    return res.json({ error: 'invalid url' });
+  }
+
+  const urlObj = new Url({
+    original_url: url,
+    short_url: nextShortUrlId.toString()
+  });
+
+  urlObj.save()
+    .then((savedUrl) => {
+      nextShortUrlId++;
+      res.json(savedUrl);
+      console.log(savedUrl)
+    })
+    .catch((error) => {
+      res.json({ error: 'Internal server error' });
+    });
+});
+
+app.get('/api/shorturl/:shortUrl', (req, res) => {
+  const { shortUrl } = req.params;
+
+  Url.findOne({ short_url: shortUrl })
+    .then((url) => {
+      if (url) {
+        res.redirect(url.original_url);
+      } else {
+        res.json({ error: 'invalid url' });
+      }
+    })
+    .catch((error) => {
+      res.json({ error: 'Internal server error' });
+    });
 });
